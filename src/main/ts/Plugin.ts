434b85e4670a931cd7daf004b8257a88690d7029
Melvin Otieno <o.melvinotieno@gmail.com>
2022-06-30 19:40:03 +0300

    use plugin options

--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -2,10 +2,21 @@ import { Editor, TinyMCE } from "tinymce";
 
 declare const tinymce: TinyMCE;
 
+interface PluginOptions {
+  title: string;
+  buttons: {
+    cancel: string;
+    insert: string;
+  };
+}
+
 const setup = (editor: Editor): void => {
+  // get plugin options - named mathlive
+  const options: PluginOptions = editor.getParam("mathlive");
+
   let markup = "";
 
-  // custom sigma icon
+  // custom sigma icon as functions
   editor.ui.registry.addIcon(
     "functions",
     '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6 20V18L12.5 12L6 6V4H18V7H10.775L16.15 12L10.775 17H18V20Z"/></svg>'
@@ -30,17 +41,17 @@ const setup = (editor: Editor): void => {
   editor.addCommand("mathlive-equation-window", function (_, data = {}) {
     editor.windowManager.openUrl({
       url: "mathlive.html",
-      title: "Insert Equation", // TODO: allow custom title
+      title: options.title || "Insert Equation",
       width: 820,
       height: 400,
       buttons: [
         {
           type: "cancel",
-          text: "Cancel", // TODO: allow custom text
+          text: options.buttons.cancel || "Cancel",
         },
         {
           type: "custom",
-          text: "Insert", // TODO: allow custom text
+          text: options.buttons.insert || "Insert",
           primary: true,
         },
       ],
@@ -54,7 +65,8 @@ const setup = (editor: Editor): void => {
         editor.windowManager.close();
       },
       onMessage: (instance, message) => {
-        if (message.mceAction === "mathlive-mounted") {
+        console.log(message);
+        if (message.mceAction === "mathlive-load") {
           instance.sendMessage({
             latex: "",
           });
