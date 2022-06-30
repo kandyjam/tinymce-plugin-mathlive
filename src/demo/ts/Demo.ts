ebb9a5a19fdf9de11c5db30d886f8e35cc65dc59
Melvin Otieno <o.melvinotieno@gmail.com>
2022-06-30 19:40:18 +0300

    add plugin options

--- src/demo/ts/Demo.ts
+++ src/demo/ts/Demo.ts
@@ -1,13 +1,20 @@
-import { TinyMCE } from 'tinymce';
+import { TinyMCE } from "tinymce";
 
-import Plugin from '../../main/ts/Plugin';
+import Plugin from "../../main/ts/Plugin";
 
 declare let tinymce: TinyMCE;
 
 Plugin();
 
 tinymce.init({
-  selector: 'textarea.tinymce',
-  plugins: 'code mathlive',
-  toolbar: 'mathlive'
+  selector: "textarea.tinymce",
+  plugins: "code mathlive",
+  toolbar: "mathlive",
+  mathlive: {
+    title: "Insert",
+    buttons: {
+      cancel: "Cancel",
+      insert: "Insert Equation",
+    },
+  },
 });
